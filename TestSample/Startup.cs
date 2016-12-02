0ac7b3a58d1064746e8c2d2e12c992d0bf5c7153
B Anil Kumar <tech1arb@gmail.com>
2016-12-02 11:20:30 +0530

    Intial code setup
    Blank state of MVC app

---
+++ TestSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestSample.Startup))]
+namespace TestSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
